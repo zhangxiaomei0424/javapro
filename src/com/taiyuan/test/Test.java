abac8d5feae1706fdcbceac965b989b593719dbe
““Zhangxiaimei0424gfig --global user.name “Zhangxiaimei0424 <2211827729@qq.com>
2020-02-07 16:17:38 +0800

    代码变了

--- src/com/taiyuan/test/Test.java
+++ src/com/taiyuan/test/Test.java
@@ -1,9 +1,25 @@
 package com.taiyuan.test;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        System.out.println(a);
+        List arraylist = new ArrayList();
+        List linkedlist = new LinkedList();
+        Date startarray = new Date();
+        for(int i=0;i<1000000;i++){
+            arraylist.add(0,i);
+        }
+        Date endarray = new Date();
+        System.out.println("ArrayList添加用时="+(endarray.getTime()-(startarray.getTime())));
+        Date startlinked = new Date();
+        for(int i=0;i<1000000;i++){
+            linkedlist.add(0,i);
+        }
+        Date endlinked = new Date();
+        System.out.println("LinkedList添加用时="+(endlinked.getTime()-(startlinked.getTime())));
     }
 }
